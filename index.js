4be27132cc4f4321abadbdd05b830f64b550e3ea
Ivan Dubovik <vanekby98@gmail.com>
2022-04-19 17:53:22 +0300

    test deploy

--- index.js
+++ index.js
@@ -14,6 +14,42 @@ async function start(){
 			useUnifiedTopology: true
 		})
 
+		app.get('/', (req, res)=>{
+			res.end(`
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<a href="/">Home</a>
+							</li>
+							<li>
+								<a href="/about">About</a>
+							</li>		
+						</ul>
+					</nav>
+					<h1>Home page</h1>
+				</div>
+			`)
+		})
+		
+		app.get('/about', (req, res)=>{
+			res.end(`
+				<div>
+					<nav>
+						<ul>
+							<li>
+								<a href="/">Home</a>
+							</li>
+							<li>
+								<a href="/about">About</a>
+							</li>		
+						</ul>
+					</nav>
+					<h1>About page</h1>
+				</div>
+			`)
+		})
+
 		app.listen(PORT, () => {
 			console.log(`Server has been started on port ${PORT}...`)
 		})
