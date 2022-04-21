3a1180b04aae4fcb3cdc58bd4c9d67d75e4efda6
Ivan Dubovik <vanekby98@gmail.com>
2022-04-21 17:43:08 +0300

    login data is saved in localstorage

---
+++ client/src/hooks/auth.hook.js
@@ -0,0 +1,34 @@
+import {useState, useCallback, useEffect} from 'react'
+
+const loginData = 'userData'
+
+export const useAuth = () => {
+	const [token, setToken] = useState(null)
+	const [userId, setUserId] = useState(null)
+
+	const login = useCallback((jwtToken, id)=> {
+		setToken(jwtToken)
+		setUserId(id)
+
+		localStorage.setItem(loginData, JSON.stringify({
+			userId: id, token: jwtToken
+		}))
+	}, [])
+
+	const logout = useCallback(()=> {
+		setToken(null)
+		setUserId(null)
+
+		localStorage.removeItem(loginData)
+	}, [])
+
+	useEffect(() => {
+		const data = JSON.parse(localStorage.getItem(loginData))
+
+		if (data && data.token){
+			login(data.token, data.userId)
+		}
+	}, [login])
+
+	return {login, logout, token, userId }
+}
