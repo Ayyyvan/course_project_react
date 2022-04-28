9a11e94d9c1d14ddb7659bd0828ac68d41c1a2ab
Ivan Dubovik <vanekby98@gmail.com>
2022-04-28 11:14:16 +0300

    fix middleware for deploy

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,5 +1,7 @@
-const jwt = require('jsonwebtoken')
 const config = require('config')
+const jwt = require('jsonwebtoken')
+const jwtSecret = process.env.JWT_SECRET || config.get('jwtSecret')
+
 
 module.exports = (req, res, next) => {
 	if (req.method === 'OPTIONS'){
@@ -7,14 +9,14 @@ module.exports = (req, res, next) => {
 	}
 
 	try{
-		
+
 		const token = req.headers.authorization.split(' ')[1]
 
 		if(!token){
 			res.status(401).json({ message: 'No authorization'})
 		}
 
-		const decoded = jwt.verify(token, config.get('jwtSecret'))
+		const decoded = jwt.verify(token, jwtSecret)
 		req.user = decoded
 		next()
 
